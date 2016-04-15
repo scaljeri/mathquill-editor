7ca472d37b91e0004727c37495fb96afd0d3dedc
Lucas Calje <lucas@frontmen.nl>
2016-04-15 10:42:20 +0200

    Move Caret to start ?

--- js/caret.js
+++ js/caret.js
@@ -21,7 +21,32 @@ window.app.Caret = (function () {
       textRange.collapse(false);
       textRange.select();
     }
-  }
+  };
+
+
+  /* http://stackoverflow.com/questions/4233265/contenteditable-set-caret-at-the-end-of-the-text-cross-browser
+   // atStart is a boolean
+   Caret.prototype.start = function (atStart) {
+   function createCaretPlacer(atStart) {
+   return function(el) {
+   el.focus();
+   if (typeof window.getSelection != "undefined"
+   && typeof document.createRange != "undefined") {
+   var range = document.createRange();
+   range.selectNodeContents(el);
+   range.collapse(atStart);
+   var sel = window.getSelection();
+   sel.removeAllRanges();
+   sel.addRange(range);
+   } else if (typeof document.body.createTextRange != "undefined") {
+   var textRange = document.body.createTextRange();
+   textRange.moveToElementText(el);
+   textRange.collapse(atStart);
+   textRange.select();
+   }
+   };
+   }
+   };*/
 
   return Caret;
 })();
